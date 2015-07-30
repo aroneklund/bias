5458ebc69f2e0fed9194c46907a267fb3ef3d2cf
Aron Eklund <eklund@cbs.dtu.dk>
2015-07-30 14:21:36 +0200

    This is version 0.0.5 as has been available from my website
    This is version 0.0.5, which has been available since 2012-03-13 from
    the website:
     http://www.cbs.dtu.dk/suppl/biascorr/

---
+++ R/bias.R
@@ -0,0 +1,261 @@
+#
+# bias.R
+# created 2007-12-05
+# Aron Eklund
+#
+#
+# 
+
+
+getBiasMetrics <- function(x.batch, x.rma = rma(x.batch)) {
+  x.pm <- log2(pm(x.batch))
+  message('Calculating degradation scores...')
+  degradation <- affy::AffyRNAdeg(x.batch)$slope
+  data.frame(
+          pm.median   = apply(x.pm, 2, median),
+          pm.IQR      = apply(x.pm, 2, IQR),
+          rma.IQR     = apply(exprs(x.rma), 2, IQR),
+          degradation = degradation
+  )
+}
+
+
+getBiasMetrics2 <- function(x.batch, x.rma = rma(x.batch)) {
+  ## first do the four original bias metrics
+  x.pm <- log2(pm(x.batch))
+  pm.median   <- apply(x.pm, 2, median)
+  pm.IQR      <- apply(x.pm, 2, IQR)
+  rma.IQR     <- apply(exprs(x.rma), 2, IQR)
+  message('Calculating degradation scores...')
+  degradation <- affy::AffyRNAdeg(x.batch)$slope
+  
+  ## now the "extended" bias metrics
+  message('Calculating MAS5 calls...')
+  present.calls <- apply(exprs(.mas5calls2(x.batch)) == 'P', 2, mean)
+
+  ## probe-specific bias metrics 
+  ## (all probe sets may not be present on all arrays -- so we use safety functions)  
+  availableProbes <- featureNames(x.rma)
+  getMeans <- function(probes) {
+    ok <- intersect(probes, availableProbes)
+    colMeans(exprs(x.rma)[ok, , drop=FALSE])
+  }
+
+  pr.spikes.mRNA <- c("AFFX-LysX-3_at", "AFFX-LysX-5_at", "AFFX-LysX-M_at",             
+                   "AFFX-PheX-3_at", "AFFX-PheX-5_at", "AFFX-PheX-M_at", 
+                   "AFFX-ThrX-3_at", "AFFX-ThrX-5_at", "AFFX-ThrX-M_at",
+                   "AFFX-DapX-3_at", "AFFX-DapX-5_at", "AFFX-DapX-M_at"  )
+  pr.spikes.cRNA <- c("AFFX-BioB-3_at", "AFFX-BioB-5_at", "AFFX-BioB-M_at",
+                   "AFFX-BioC-3_at", "AFFX-BioC-5_at", 
+                   "AFFX-BioDn-3_at", "AFFX-BioDn-5_at",
+                   "AFFX-CreX-3_at", "AFFX-CreX-5_at"   )
+  pr.rRNA <- c("AFFX-HUMRGE/M10098_3_at", "AFFX-HUMRGE/M10098_5_at", 
+            "AFFX-HUMRGE/M10098_M_at", "AFFX-M27830_5_at", "AFFX-M27830_M_at")
+  pr.alu <- "AFFX-hum_alu_at"
+  
+  rma.spikes.mRNA <- getMeans(pr.spikes.mRNA)
+  rma.spikes.cRNA <- getMeans(pr.spikes.cRNA)
+  rma.rRNA <- getMeans(pr.rRNA)
+  rma.alu <- getMeans(pr.alu)
+  
+  border.plus  <- sapply(borders(x.batch), function(x) median(unlist(x$plus)))
+  border.minus <- sapply(borders(x.batch), function(x) median(unlist(x$minus)))
+
+  data.frame( 
+    pm.median, pm.IQR, rma.IQR, degradation,
+    present.calls, 
+    rma.spikes.mRNA, rma.spikes.cRNA, 
+    rma.rRNA, rma.alu,
+    border.plus, border.minus
+  )
+}
+
+
+borders <- function(x.batch) {
+  nr <- nrow(x.batch)
+  nc <- ncol(x.batch)
+  xy2i <- function(x, y) x + (nr * (y - 1))
+  sb <- (nr %% 2)  # sign on bottom
+  sr <- (nc %% 2)  # sign on right
+  index.plus <- list( bottom = xy2i(seq(1 + sb, nc, by=2), nr),
+                        left = xy2i(1, seq(1, nr, by=2)),
+                         top = xy2i(seq(1, nc, by=2), 1), 
+                       right = xy2i(nc, seq(1 + sr, nr, by=2))  )
+  index.minus <- list(bottom = xy2i(seq(2 - sb, nc, by=2), nr),
+                        left = xy2i(1, seq(2, nr, by=2)),
+                         top = xy2i(seq(2, nc, by=2), 1), 
+                       right = xy2i(nc, seq(2 - sr, nr, by=2))  )
+  out <- lapply(1:ncol(exprs(x.batch)), function(j) {
+    plus  <- lapply(index.plus,  function(i) exprs(x.batch)[i, j])    
+    minus <- lapply(index.minus, function(i) exprs(x.batch)[i, j])    
+    list(plus = plus, minus = minus)
+  })
+  names(out) <- sampleNames(x.batch)
+  out
+}
+
+
+biasCorrection <- function(x, metrics) {
+  if( class(x) %in% c('exprSet', 'ExpressionSet')) {
+    in.mat <- Biobase::exprs(x)
+  } else {
+    in.mat <- x
+  }
+  fit <- lm(t(in.mat) ~ ., data = metrics)
+  res <- t(residuals(fit))
+  fixed <- sweep(res, 1, rowMeans(in.mat), FUN = '+')
+  if( class(x) %in% c('exprSet', 'ExpressionSet')) {
+    out <- x
+    Biobase::exprs(out) <- fixed
+  } else {
+    out <- fixed
+  }
+  return(out)
+}
+
+
+
+getScanDate <- function(filenames = list.celfiles()) {
+  DatHeaders <- rep('', length(filenames))
+  for (i in seq(along = filenames)) {
+    con <- gzfile(filenames[i])
+    DatHeaders[i] <- grep('DatHeader', readLines(con, n = 20), value = TRUE)
+    close(con)
+  }
+  dateInUSAformat <- sub("^.*(../../..).*$", "\\1", DatHeaders, perl = TRUE)
+  dateInDateFormat <- as.Date(dateInUSAformat, "%m/%d/%y")
+  names(dateInDateFormat) <- filenames
+  dateInDateFormat
+}
+
+
+calcCM <- function(x, n = 50, 
+       use = 'all', method = 'pearson', 
+       FUN = median, ...) {
+  if( class(x) %in% c('exprSet', 'ExpressionSet')) 
+    x <- Biobase::exprs(x)
+  x <- t(as.matrix(x))
+  b <- round(seq(0, ncol(x), length = n + 1))
+  z <- matrix(NA, nrow = n, ncol = n)
+  count <- matrix(NA, nrow = n, ncol = n)
+  for (i in 1:n) {
+    which.i <- (b[i] + 1):b[i + 1]
+    for (j in i:n) {
+      which.j <- (b[j] + 1):b[j + 1]
+      myCor <- cor( x[,which.i], x[, which.j], 
+                    use = use, method = method )
+      if( i == j ) { 
+        myCor <- myCor[lower.tri(myCor)]
+        z[i,j] <- FUN(myCor, ...) 
+        count[i,j] <- length(myCor) 
+      } else {
+        z[i,j] <- z[j,i] <- FUN(myCor, ...)
+        count[i,j] <- count[j,i] <- length(myCor)
+      }
+    }
+  }
+  return(list(x = b, y = b, z = z, count = count, 
+    call = paste( as.character(match.call()), collapse=" " ) ))
+}
+
+
+
+quantileNormalize <- function(x, margin = 2) {
+  v <- rowMeans(apply(x, margin, sort))
+  x.qn <- apply(x, margin, function(y) v[rank(y, ties.method = 'random')] )
+  rownames(x.qn) <- rownames(x)
+  return(x.qn)
+}
+
+
+batchCorrection <- function(x, b) {
+    stopifnot( all( ! is.na(b) ) )
+    stopifnot(length(b) == ncol(x))
+    if (class(x) %in% c("exprSet", "ExpressionSet")) {
+        in.mat <- Biobase::exprs(x)
+    }
+    else {
+        in.mat <- x
+    }
+    b <- factor(b)
+    batchMeans <- sapply(levels(b), function(a) 
+                     rowMeans(in.mat[,b == a, drop = FALSE]) )
+    adj <- batchMeans - rowMeans(in.mat)
+    fixed <- in.mat - adj[,b]
+    if (class(x) %in% c("exprSet", "ExpressionSet")) {
+        out <- x
+        Biobase::exprs(out) <- fixed
+    }
+    else {
+        out <- fixed
+    }
+    out
+}
+
+## modified from "mas5calls.AffyBatch" in the "affy" package
+.mas5calls2 <-
+function (object, ids = NULL, verbose = TRUE, tau = 0.015, alpha1 = 0.04, 
+    alpha2 = 0.06, ignore.saturated = TRUE) 
+{
+    if (alpha1 < 0) {
+        stop("alpha1 must be  > 0 ")
+    }
+    if (alpha1 > alpha2) {
+        stop("alpha2 must be  > alpha1 ")
+    }
+    if (alpha2 > 1) {
+        stop("alpha2 must be  <1 ")
+    }
+    if (verbose) 
+        cat("Getting probe level data...\n")
+    pms <- as.matrix(affy::pm(object))
+    mms <- as.matrix(affy::mm(object))
+    if (ignore.saturated) {
+        sat <- 46000
+    }
+    else {
+        sat <- -1
+    }
+    pns <- affy::probeNames(object)
+    o <- order(pns)
+    pns <- pns[o]
+    pms <- pms[o, , drop = FALSE]
+    mms <- mms[o, , drop = FALSE]
+    unique.pns <- sort(unique(pns))
+
+    ## what if there are no MMs?
+    if(all(is.na(mms))) {
+      calls <- matrix(NA, nrow = length(unique.pns), ncol = ncol(pms))
+      rownames(calls) <- unique.pns
+      colnames(calls) <- Biobase::sampleNames(object)
+      p <- calls
+    } else {
+      if (verbose) 
+          cat("Computing p-values\n")
+      p <- sapply(1:length(pms[1, ]), function(x) {
+          .C("DetectionPValue", as.double(pms[, x]), as.double(mms[, 
+              x]), as.character(pns), as.integer(length(mms[, x])), 
+              as.double(tau), as.double(sat), dpval = double(length(unique.pns)), 
+              length(unique.pns), PACKAGE = "affy")$dpval
+      })
+      rownames(p) <- unique.pns
+      colnames(p) <- Biobase::sampleNames(object)
+      if (verbose) 
+          cat("Making P/M/A Calls\n")
+      calls <- ifelse(p < alpha1, "P", ifelse(p < alpha2, "M", "A"))
+      calls <- matrix(calls, nrow = nrow(p), ncol = ncol(p))
+      colnames(calls) <- sampleNames(object)
+      rownames(calls) <- rownames(p)
+    }
+    if (!is.null(ids)) {
+      calls <- calls[ids, , drop = FALSE]
+      p <- p[ids, , drop = FALSE]
+    }
+    eset <- new("ExpressionSet", 
+        phenoData = Biobase::phenoData(object), 
+        experimentData = Biobase::experimentData(object), 
+        annotation = Biobase::annotation(object), 
+        protocolData = Biobase::protocolData(object), 
+        exprs = calls, se.exprs = p)
+    return(eset)
+}
